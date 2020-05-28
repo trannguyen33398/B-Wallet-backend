808a53e3cb9b57712dfe9a5487a8711e21ea1cb9
ngô nguyễn <trannguyen3398@.com>
2020-05-28 19:10:54 +0700

    fix code

--- app.js
+++ app.js
@@ -3,17 +3,48 @@ var app = express();
 var Wallet = require('ethereumjs-wallet');
 var EthUtil = require('ethereumjs-util');
 const crypto = require('crypto');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
 class Transaction {
    
   constructor(fromAddress, toAddress, amount) {
     this.fromAddress = fromAddress;
     this.toAddress = toAddress;
     this.amount = amount;
-    //this.timestamp = Date.now();
+    this.timestamp = Date.now();
   }
 
   calculateHash() {
     return crypto.createHash('sha256').update(this.fromAddress + this.toAddress + this.amount + this.timestamp).digest('hex');
+  }
+  signTransaction(signingKey) {
+    // You can only send a transaction from the wallet that is linked to your
+    // key. So here we check if the fromAddress matches your publicKey
+    if (signingKey.getPublic('hex') !== this.fromAddress) {
+      throw new Error('You cannot sign transactions for other wallets!');
+    }
+    
+
+    // Calculate the hash of this transaction, sign it with the key
+    // and store it inside the transaction obect
+    const hashTx = this.calculateHash();
+    const sig = signingKey.sign(hashTx, 'base64');
+
+    this.signature = sig.toDER('hex');
+  }
+  isValid() {
+    // If the transaction doesn't have a from address we assume it's a
+    // mining reward and that it's valid. You could verify this in a
+    // different way (special field for instance)
+    if (this.fromAddress === null) return true;
+
+    if (!this.signature || this.signature.length === 0) {
+      throw new Error('No signature in this transaction');
+    }
+
+    const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
   }
 }
 
@@ -41,15 +72,15 @@ class Block {
     console.log('Block mined:' +this.hash);
   }
 
-  // hasValidTransactions() {
-  //   for (const tx of this.transactions) {
-  //     if (!tx.isValid()) {
-  //       return false;
-  //     }
-  //   }
+  hasValidTransactions() {
+    for (const tx of this.transactions) {
+      if (!tx.isValid()) {
+        return false;
+      }
+    }
 
-  //   return true;
-  // }
+    return true;
+  }
 }
 
 class Blockchain {
@@ -57,7 +88,7 @@ class Blockchain {
     this.chain = [this.createGenesisBlock()];
     this.difficulty = 4;
     this.pendingTransactions = [];
-    this.miningReward = 15;
+    this.miningReward = 100;
   }
 
   createGenesisBlock() {
@@ -88,34 +119,32 @@ class Blockchain {
     }
 
     // Verify the transactiion
-    // if (!transaction.isValid()) {
-    //   throw new Error('Cannot add invalid transaction to chain');
-    // }
+    if (!transaction.isValid()) {
+      throw new Error('Cannot add invalid transaction to chain');
+    }
     
-    // if (transaction.amount <= 0) {
-    //   throw new Error('Transaction amount should be higher than 0');
-    // }
+    if (transaction.amount <= 0) {
+      throw new Error('Transaction amount should be higher than 0');
+    }
     
-    // Making sure that the amount sent is not greater than existing balance
-  //   if (this.getBalanceOfAddress(transaction.fromAddress) < transaction.amount) {
-  //     throw new Error('Not enough balance');
-  //   }
+ 
 
     this.pendingTransactions.push(transaction);
     console.log('transaction added: %s', transaction);
   }
 
 
-  getBalanceOfAddress(wallet) {
-    let balance = wallet.amount;
+  getBalanceOfAddress(publickey) {
+    let balance = 0
+    ;
 
     for (const block of this.chain) {
       for (const trans of block.transactions) {
-        if (trans.fromAddress === wallet.address) {
+        if (trans.fromAddress === publickey) {
           balance -= trans.amount;
         }
 
-        if (trans.toAddress === wallet.address) {
+        if (trans.toAddress === publickey) {
           balance += trans.amount;
         }
       }
@@ -155,9 +184,9 @@ class Blockchain {
     for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i];
 
-      // if (!currentBlock.hasValidTransactions()) {
-      //   return false;
-      // }
+      if (!currentBlock.hasValidTransactions()) {
+        return false;
+      }
 
       if (currentBlock.hash !== currentBlock.calculateHash()) {
         return false;
@@ -167,31 +196,42 @@ class Blockchain {
     return true;
   }
 }
-// let a={
-//   'address':'a',
-//   'amount': 1000
-// };
-// let b={
-//   'address':'b',
-//   'amount': 100
-// };
-// let c={
-//   'address':'c',
-//   'amount': 0
-// };
-//  let bin =new Blockchain();
-//  bin.addTransaction(new Transaction(a.address,b.address,100));
-//  bin.addTransaction(new Transaction(a.address,b.address,400));
-//  bin.minePendingTransactions(c.address);
-//  console.log('của tao nè :',bin.getBalanceOfAddress(c));
+// var accountPassword="abcd";
+// var key=Wallet.generate(accountPassword);
+// var privateKey=key._privKey;
+// var wallet=Wallet.fromPrivateKey(privateKey);
+// privateKey=privateKey.toString('hex');
+// var keyStoreData=wallet.toV3(accountPassword);
+// const keystoreFilename = wallet.getV3Filename();
+// const publicKey = wallet.getPublicKeyString();
+
+// const address = wallet.getAddressString();
+ const myKey=ec.keyFromPrivate('bffe98de4ffdc49d83c57925528f95996e5836ad8677bad237c72db5a8284e40');
+ const public=myKey.getPublic('hex');
  
-//  console.log('tiền của a sau ck :',bin.getBalanceOfAddress(a));
-//  console.log('tiền của b sau ck :',bin.getBalanceOfAddress(b));
-//  var transHistory=bin.getAllTransactionsForWallet(a.address);
-//  for(let i of transHistory) console.log(i,'\n');
+
+
+ let bin =new Blockchain();
+ const trans1=new Transaction(public,'b',10);
+ trans1.signTransaction(myKey);
+ bin.addTransaction(trans1);
+ const trans2=new Transaction(public,'c',20);
+ trans2.signTransaction(myKey);
+ bin.addTransaction(trans2);
+//  const trans2=new Transaction(public,'c',400);
+//  trans2.signTransaction(myKey);
+//  bin.addTransaction(trans2);
+ bin.minePendingTransactions('c');
+ 
+ 
+ console.log('tiền của a sau ck :',bin.getBalanceOfAddress(public));
+ console.log('tiền của b sau ck :',bin.getBalanceOfAddress('b'));
+ console.log('tiền của c sau ck :',bin.getBalanceOfAddress('c'));
+ var transHistory=bin.getAllTransactionsForWallet(public);
+ for(let i of transHistory) console.log(i,'\n');
 //--------------------------------------------------------------------------------------------------
 
-// var privateKey='0x61ce8b95ca5fd6f55cd97ac60817777bdf64f1670e903758ce53efc32c3dffeb';
+// var privateKey='061ce8b95ca5fd6f55cd97ac60817777bdf64f1670e903758ce53efc32c3dffeb';
 
 // const privateKeyBuffer = EthUtil.toBuffer(privateKey);
 // const wallet = Wallet.fromPrivateKey(privateKeyBuffer);
@@ -206,10 +246,9 @@ class Blockchain {
 
 // const address = wallet.getAddressString();
 
-// console.log(privateKey);
-// console.log(address);
-//  console.log(keyStoreData);
-
+//console.log(privateKey);
+//console.log(address);
+ 
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
